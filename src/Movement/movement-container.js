256e750a521e57be5bd73bdf9f105aa48c5776f1
Maxx Lehmann <mglehman@mtu.edu>
2019-05-03 21:47:01 +0200

    print from kurt

--- src/Movement/movement-container.js
+++ src/Movement/movement-container.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Context } from '../global-context';
+import kurt from '../kurt'
 
 export default class Movement extends React.Component {
   static contextType = Context
 
   render() {
+    const { x, y, z } = this.context.acceleration
+    const out = kurt(x, y, z)
+
     return (
       <div>
-        <div>x: {this.context.acceleration.x}</div>
-        <div>y: {this.context.acceleration.y}</div>
-        <div>z: {this.context.acceleration.z}</div>
+        {Object.keys(out).map(k => <div key={k}>{k}: {out[k]}</div>)}
       </div>
     )
   }
