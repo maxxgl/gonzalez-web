75b35bc1882dee6d9b480775e9a0b27da179cd5b
Kurt Lehmann <kurt@Kurts-MacBook-Pro.local>
2019-05-04 15:54:33 +0200

    fixed vale accessor

--- src/kurt.js
+++ src/kurt.js
@@ -1,8 +1,7 @@
 export default function(accel, timestamp, data) {
 //accel.x... are defined
 
-  let orthog = Math.pow((Math.pow(accel.x, 2) + Math.pow(accel.y, 2) + Math.pow(accel.y, 2)) , 0.5)
-  
+  let orthog = Math.pow((Math.pow(accel.x, 2) + Math.pow(accel.y, 2) + Math.pow(accel.z, 2)) , 0.5)
 
   return { x: accel.x, y: accel.y, z: accel.z, orthog: orthog }
 
