02bab1c5209404e472e90acda0b033d107384b10
Kurt Lehmann <klehmann00@yahoo.com>
2019-05-05 11:29:30 +0200

    update test for new outputs

--- src/kurt.test.js
+++ src/kurt.test.js
@@ -1,11 +1,14 @@
 import kurt from './kurt'
 
 it('Outputs expected values', () => {
-  const values = { x: 2, y: 3, z: 1}
-  const result = kurt(values, 0, [])
 
-  if (result.x !== 2 || result.y !== 3 || result.z !== 1 || result.orthog !== Math.pow(14, .5)) {
-    console.log(result)
-    throw new Error("Values do not match")
+  const values = { x: 2, y: 3, z: 1}
+  const result = kurt(values, 0, {})[0]
+
+  const calc = Math.pow(14, .5)
+
+  if (result.orthog !== calc) {
+    throw new Error(`Orthog mismatch. Actual: ${result.orthog}, Expected: ${calc}`)
   }
+
 });
