3992d78e09046837d0e7a223a69b07a7b861640e
Kurt Lehmann <klehmann00@yahoo.com>
2019-05-05 11:46:06 +0200

    sawtooth counter

--- src/kurt.js
+++ src/kurt.js
@@ -9,7 +9,7 @@ export default function(accel, timestamp, log, count) {
 //log.x,y,z,
 //timestamp available
 
-  let newCount = count+1
+  let newCount = count + 0.01
   if (newCount > 10) {
     newCount = 0
   }
@@ -44,6 +44,6 @@ export default function(accel, timestamp, log, count) {
 
   let orthog = Math.pow((Math.pow(accelx, 2) + Math.pow(accely, 2) + Math.pow(accelz, 2)) , 0.5)
 
-  return [{ accelx, accely, accelz, orthog, filt_accelx }, newCount]
+  return [{ accelx, accely, accelz, orthog, filt_accelx, newCount }, newCount]
 
 }
